59b533f4211f20c0878a0349a4c9c3586588d9a1
siyam04 <galib.abdullah04@gmil.com>
2021-02-05 22:41:15 +0600

    initial form added

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,145 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+
+
+    <form class="container mt-5 border border-warning">
+      <h1 class="mb-5 mt-3" style="color: #ff0090">Dushanbe</h1>
+      <!--      Bill-->
+      <div class="form-group">
+        <label>Bill</label>
+        <select class="form-control">
+          <option selected disabled>Select Bill</option>
+          <option v-for="bill in all_bills" :key="bill.id"
+                  :value="bill.id">
+            {{ bill.bill_name }}
+          </option>
+        </select>
+      </div>
+      <!--      Topics-->
+      <div class="form-group">
+        <label>Topic</label>
+        <select class="form-control">
+          <option selected disabled>Select Topic</option>
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </select>
+      </div>
+
+      <!--      Materials-->
+      <div class="position-relative form-group">
+        <label>Materials</label>
+        <select class="form-control">
+          <option selected disabled>Select Materials</option>
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </select>
+
+        <div class="form-row mt-3">
+          <div class="col-md-4">
+            <div class="position-relative form-group">
+
+              <input
+                  placeholder="Serial Number"
+                  id="department_code"
+                  v-model="department_code"
+                  type="text"
+                  disabled
+                  class="form-control"
+              />
+
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="position-relative form-group">
+              <input
+                  disabled
+                  placeholder="Unit"
+                  v-model="department_code"
+                  type="text"
+                  class="form-control"
+              />
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="form-group">
+              <input
+                  disabled
+                  placeholder="Quantity"
+                  v-model="department_code"
+                  type="text"
+                  class="form-control"
+              />
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+
+      <!--      work_progress-->
+      <div class="position-relative form-group">
+        <label>work progress</label>
+        <input
+            placeholder="0"
+            id="department_name"
+            v-model="department_name"
+            type="number"
+            class="form-control"/>
+      </div>
+
+      <button type="button" class="btn btn-success btn-lg mb-4">Submit</button>
+
+    </form>
   </div>
 </template>
 
 <script>
+import axios from "axios"
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  data() {
+    return {
+      all_bills: null
+    }
+  },
+
+
+  methods: {
+    LoadBill: function () {
+      // const token = localStorage.getItem("token");
+      axios
+          .get("http://galib04.pythonanywhere.com/api/bills/", {
+            // headers: {
+            //   Authorization: token ${token},
+            // },
+          })
+          .then(
+              function (response) {
+                this.all_bills = response.data;
+              }.bind(this)
+          );
+    },
+
+  },
+
+  created() {
+    this.LoadBill();
+  },
+
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
